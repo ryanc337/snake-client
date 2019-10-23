5e3cf266656c886cfd1a7fb33267c468e403957d
Ryan <cordingley.ryan@gmail.com>
2019-10-23 22:47:51 +0000

    committed client and constants

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.149',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // notifies connection
   conn.on('connect', () => {
