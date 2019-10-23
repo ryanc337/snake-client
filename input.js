799c068f84a77e06c453534ced6957040002d756
Ryan <cordingley.ryan@gmail.com>
2019-10-23 21:38:06 +0000

    added input file

---
+++ input.js
@@ -0,0 +1,16 @@
+function setupInput () {
+  const stdin = process.stdin;
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+function handleUserInput(key) {
+  if (key === "\u0003") {
+  process.exit();
+}
+};
+
+module.exports = { setupInput }
--- play.js
+++ play.js
@@ -1,26 +1,10 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-
-function setupInput () {
-  const stdin = process.stdin;
-  stdin.on('data', handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-};
-
-function handleUserInput(key) {
-    if (key === "\u0003") {
-    process.exit();
-  }
-};
-
 setupInput();
-
 
 
 
